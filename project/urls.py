22e83c29729c6a3e357318060d006388e8d0095b
[TahaAlothman] <tahaaborawi@gmail.com>
2023-10-05 21:18:23 +0200

    add home

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path ,include
 
 from django.conf import settings
+from settings.views import home
 from django.conf.urls.static import static
 
 
@@ -25,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("products/", include('products.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('',home)
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from products.models import Product,Brand,Review
 
-# Create your views here.
+
+
+
+
+def home(request):
+    brands = Brand.objects.all()[:20]
+    sale_products = Product.objects.filter(flag = 'Sale')[:10]
+    feature_products = Product.objects.filter(flag = 'Feature')[:10]
+    new_products = Product.objects.filter(flag = 'New')[:10]
+    reviews = Review.objects.all()[:5]
+
+    return render(request,'settings/home.html',{'brands':brands,'sale_products':sale_products,
+                                                'feature_products':feature_products,'new_products':new_products,'reviews':reviews})
