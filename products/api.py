77234317d307c61f7c0407e23c9d274859382d82
[TahaAlothman] <tahaaborawi@gmail.com>
2023-10-06 21:29:23 +0200

    add product detail api

--- products/api.py
+++ products/api.py
@@ -6,5 +6,13 @@ from .models import Product
 @api_view(['GET'])
 def product_list_api(request):
     products = Product.objects.all()   #list
-    data = ProductSerializer(products,many=True).data   #json
+    data = ProductSerializer(products,many=True,context={'request':request}).data   #json
+    return Response({'products':data})
+
+
+
+@api_view(['GET'])
+def product_detail_api(request,product_id):
+    product =  Product.objects.get(id = product_id)
+    data = ProductSerializer(product,context={'request':request}).data   #json
     return Response({'products':data})
