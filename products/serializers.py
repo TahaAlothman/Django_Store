6a8bb23704aff0bc8fa54c0541a7edbdab125e20
[TahaAlothman] <tahaaborawi@gmail.com>
2023-10-07 18:12:50 +0200

    show avg rate in product detail

--- products/serializers.py
+++ products/serializers.py
@@ -17,6 +17,7 @@ class ReviewSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True,source = 'review_product')
     reviews_count = serializers.SerializerMethodField()
+    avg_rate = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
@@ -24,6 +25,18 @@ class ProductSerializer(serializers.ModelSerializer):
     def get_reviews_count (self,object):
         reviews_count =object.review_product.all().count()
         return reviews_count
+
+
+    def get_avg_rate(self,object):
+        total = 0
+        reviews = object.review_product.all()
+        for review in reviews:
+            total += review.rate
+        
+        return total/len(reviews)
+
+
+
 
 class BrandSerializer(serializers.ModelSerializer):
     producsts = ProductSerializer(many = True,source = "product_brand")
