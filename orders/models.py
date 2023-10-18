24646d28432ec82a91886592bf0d0290df8ca5cc
[TahaAlothman] <tahaaborawi@gmail.com>
2023-10-18 17:26:44 +0200

    add order to modols

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from utils.generate_code import generate_code
 
-# Create your models here.
+ORDER_STATUS =(('Recieved','Recieved'),('Processed','Processed'),('Shipped','Shipped'),('Delivered','Delivered'))
+class Order(models.Model):
+    user = models.ForeignKey(User,related_name='order_owner',on_delete=models.SET_NULL,null=True,blank=True)
+    status = models.CharField(max_length=12,choices=ORDER_STATUS,default='Recieved')
+    code = models.CharField(max_length=10,default=generate_code)
+    order_time = models.DateTimeField(default=timezone.now)
+    delivery_time= models.DateTimeField(null=True,blank=True)
+
+
+
+
+
+class OrderDetail(models.Model):
+    pass
