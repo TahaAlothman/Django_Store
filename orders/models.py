16e8a6950ac8b2681da4a1e822ddbaff093b4aec
[TahaAlothman] <tahaaborawi@gmail.com>
2023-10-19 14:52:24 +0200

    add class Coupon

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from utils.generate_code import generate_code
 from products.models import Product
+import datetime
+from utils.generate_code import generate_code
+
 
 ORDER_STATUS =(('Recieved','Recieved'),('Processed','Processed'),('Shipped','Shipped'),('Delivered','Delivered'))
 class Order(models.Model):
@@ -11,6 +13,9 @@ class Order(models.Model):
     code = models.CharField(max_length=10,default=generate_code)
     order_time = models.DateTimeField(default=timezone.now)
     delivery_time= models.DateTimeField(null=True,blank=True)
+    coupon = models.ForeignKey('Coupon',related_name='order_coupon',on_delete=models.SET_NULL,null=True,blank=True)
+    order_total_discount= models.FloatField(null=True,blank=True)
+
 
 
 
@@ -26,4 +31,14 @@ class OrderDetail(models.Model):
 
 
 
-class 
+class Coupon (models.Model):
+    code = models.CharField(max_length=12)
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField (null=True,blank=True)
+    quantity = models.IntegerField()
+    discount= models.FloatField()
+
+    def save(self, *args, **kwargs):
+       week = datetime.timedelta(days=7)
+       self.end_date= self.start_date + week
+       super(Coupon, self).save(*args, **kwargs) # Call the real save() method
