4b72b15746161a3d2e7d5bc072c31fc341a1c467
[TahaAlothman] <tahaaborawi@gmail.com>
2023-10-19 14:09:23 +0200

    add class orderdetail to modols

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from utils.generate_code import generate_code
+from products.models import Product
 
 ORDER_STATUS =(('Recieved','Recieved'),('Processed','Processed'),('Shipped','Shipped'),('Delivered','Delivered'))
 class Order(models.Model):
@@ -16,4 +17,13 @@ class Order(models.Model):
 
 
 class OrderDetail(models.Model):
-    pass
+     order = models.ForeignKey(Order,related_name='order_detail',on_delete=models.CASCADE)
+     product = models.ForeignKey(Product,related_name='orderdetail_product',on_delete=models.SET_NULL,null=True,blank=True)
+     quantity = models.IntegerField()
+     price = models.FloatField()
+     total = models.FloatField()
+
+
+
+
+class 
