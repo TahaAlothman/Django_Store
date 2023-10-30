a7b47085693b58e46678eff206e33caa959d2274
[TahaAlothman] <tahaaborawi@gmail.com>
2023-10-30 18:35:32 +0100

    checkout setup

--- orders/views.py
+++ orders/views.py
@@ -11,5 +11,5 @@ def order_list(request):
 
 def checkout(request):
     cart =Cart.objects.get(user=request.user,status='inprogress')
-    cart_detail = Cart.objects.get(cart=cart)
+    cart_detail = CartDetail.objects.filter(cart=cart)
     return render(request,'orders/checkout.html',{'cart': cart,'cart_detail':cart_detail})
