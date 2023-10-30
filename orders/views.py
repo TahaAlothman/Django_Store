3c4f7caf1661c149235bc51be4e2b525098c0aca
[TahaAlothman] <tahaaborawi@gmail.com>
2023-10-30 19:04:46 +0100

    add total and subtotal in checkout page

--- orders/views.py
+++ orders/views.py
@@ -13,8 +13,14 @@ def checkout(request):
     cart =Cart.objects.get(user=request.user,status='inprogress')
     cart_detail = CartDetail.objects.filter(cart=cart)
     delivery_fee = DeliveryFee.objects.last().fee
+    sub_total =cart.cart_total()
+    total = sub_total + delivery_fee
+
+
     return render(request,'orders/checkout.html',{
         'cart': cart,
         'cart_detail':cart_detail,
-        'delivery_fee':delivery_fee
+        'delivery_fee':delivery_fee,
+        'sub_total':sub_total,
+        'total': total
         })
