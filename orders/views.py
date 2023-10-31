53276f06f2e5109335f70ad914e7513da24b3b68
[TahaAlothman] <tahaaborawi@gmail.com>
2023-10-31 16:24:46 +0100

    apply coupon code in checkout

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,7 @@
+import datetime
 from django.shortcuts import render
-from .models import Order,OrderDetail,Cart,CartDetail
+from django.shortcuts import get_object_or_404
+from .models import Order,OrderDetail,Cart,CartDetail, Coupon
 from settings.models import DeliveryFee
 
 
@@ -16,6 +18,35 @@ def checkout(request):
     sub_total =cart.cart_total()
     total = sub_total + delivery_fee
     discount = 0
+
+    if request.method == 'POST':
+        code = request.POST['coupon_code']  # <==> code = request.POST.get('coupon_code)
+        coupon = get_object_or_404(Coupon , code=code) # <==> coupon = Coupon.objects.get(code=code) ..wenn wir kein Coupon haben dann bekommen wir cruch und das wird ausfallen
+        if coupon and coupon.quantity > 0 :
+            today_date = datetime.datetime.today().date()
+            if today_date >= coupon.start_date and today_date <= coupon.end_date:
+                coupon_value = sub_total / 100*coupon.discount
+                sub_total = sub_total - coupon_value
+                total = sub_total + delivery_fee
+                
+                cart.coupon = coupon
+                cart.order_total_discount = sub_total
+                coupon.quantity -= 1
+                cart.save()
+                coupon.save()
+                
+                return render(request,'orders/checkout.html',{
+                    'cart' : cart , 
+                    'cart_detail': cart_detail , 
+                    'delivery_fee': delivery_fee , 
+                    'sub_total': round(sub_total,2) , 
+                    'total':total , 
+                    'discount':round(coupon_value,2)
+                })               
+        
+        
+
+
 
 
     return render(request,'orders/checkout.html',{
