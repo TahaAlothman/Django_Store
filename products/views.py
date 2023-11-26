88e024a661f67172b4a444ba5891eea7d2d6c56d
[TahaAlothman] <tahaaborawi@gmail.com>
2023-11-26 17:53:11 +0100

    show productsin debug.html ,The products show,the price of thim is more than 90

--- products/views.py
+++ products/views.py
@@ -7,7 +7,8 @@ from .models import Product , ProductImage ,  Review ,Brand
 
 
 def mydebug(request):
-    data = Product.objects.all()
+    #data = Product.objects.all()
+    data = Product.objects.filter(price__gt=90)
     return render(request,'products/debug.html',{'data':data})
 
 
