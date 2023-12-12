0495db3eaeebe91a3c70f2c4fd68bf5dc28eebac
[TahaAlothman] <tahaaborawi@gmail.com>
2023-12-12 14:26:45 +0100

    add creat_profile to models(If the user logs in and his ACC is saved, a profile will be created for him)

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from utils.generate_code import generate_code
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User , related_name='user_profile',on_delete=models.CASCADE)
@@ -9,7 +10,12 @@ class Profile(models.Model):
     code = models.CharField(max_length=10 ,default=generate_code)
 
 
-
+@receiver( post_save,sender=User)
+def create_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance
+        )
 
 
 
