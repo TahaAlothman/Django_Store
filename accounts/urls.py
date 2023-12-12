d242d5c6249c0a166ad4b13aa6bcc75fe6418696
[TahaAlothman] <tahaaborawi@gmail.com>
2023-12-12 15:08:59 +0100

    add urls

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+from .views import signup , activate , dashboard
+
+app_name = 'accounts'
+
+
+
+urlpatterns = [
+    path('signup' , signup),
+    path('dashboard',dashboard),
+    path('<str:username>/activate' , activate) ,
+]
