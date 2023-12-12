2d5900ba6c592f1b3381b73a2c920ee755d7c85a
[TahaAlothman] <tahaaborawi@gmail.com>
2023-12-12 14:17:39 +0100

    create accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -7,3 +7,34 @@ class Profile(models.Model):
     user = models.OneToOneField(User , related_name='user_profile',on_delete=models.CASCADE)
     image = models.ImageField(upload_to='accounts')
     code = models.CharField(max_length=10 ,default=generate_code)
+
+
+
+
+
+
+NUMBER_TYPES = (
+    ('Primary', 'Primary'),
+    ('Secondary' , 'Secondary')
+)
+class ContactNumbers(models.Model):
+    user = models.ForeignKey(User,related_name='user_phones',on_delete=models.CASCADE)
+    type = models.CharField(max_length=10,choices=NUMBER_TYPES)
+    number = models.CharField(max_length=25)
+
+
+
+
+
+ADDRESS_TYPES = (
+    ('Home','Home'),
+    ('Office','Office'),
+    ('Bussines','Bussines'),
+    ('Academy','Academy'),
+    ('Other','Other'),
+)
+
+class Address(models.Model):
+    user = models.ForeignKey(User,related_name='user_address',on_delete=models.CASCADE)
+    type = models.CharField(max_length=10,choices=ADDRESS_TYPES)
+    address = models.CharField(max_length=150)
