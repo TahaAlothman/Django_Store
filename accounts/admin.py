8298388371214fe42681277873f5e12125154f9a
[TahaAlothman] <tahaaborawi@gmail.com>
2023-12-12 14:19:11 +0100

    design admin panel in accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile , Address , ContactNumbers
+
+
+
+admin.site.register(Profile)
+admin.site.register(Address)
+admin.site.register(ContactNumbers)
