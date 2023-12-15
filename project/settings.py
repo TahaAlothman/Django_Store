506a989f7411fc3da4ee2cb0be9387222cdd95a9
[TahaAlothman] <tahaaborawi@gmail.com>
2023-12-15 17:54:35 +0100

    add postgres to setting

--- project/settings.py
+++ project/settings.py
@@ -103,9 +103,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "amazon",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
