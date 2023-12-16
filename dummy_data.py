09bfe6279c48b4173b2fd82b40d136be37af29ae
[TahaAlothman] <tahaaborawi@gmail.com>
2023-12-16 20:06:24 +0100

    fix code add postgres and create data

--- dummy_data.py
+++ dummy_data.py
@@ -19,7 +19,7 @@ def add_brands(n):
         )
 
     print(f'{n} brands was created successfully')
-
+#add_brands(2000)
 def add_products(n):
      fake = Faker()
      images=['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg','09.jpg','10.jpg']
@@ -38,7 +38,7 @@ def add_products(n):
 
         )
      print(f'{n} products was created successfully')
-
+#add_products(1000)
 def add_reviews(n):
     fake = Faker()
     for x in range(n):
@@ -51,5 +51,5 @@ def add_reviews(n):
         )
     print(f'{n} reviews was created successfully')
 
-add_reviews(100)
+add_reviews(3000)
 
